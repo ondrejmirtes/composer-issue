234a9f4c733f06583f33c4d43c54ea89dfd4a5ad
Ondrej Mirtes <ondrej@mirtes.cz>
2021-12-22 17:09:30 +0100

    Test

---
+++ src/promoted-properties-types.php
@@ -0,0 +1,105 @@
+<?php // lint >= 8.0
+
+namespace PromotedPropertiesTypes;
+
+use function PHPStan\Testing\assertNativeType;
+use function PHPStan\Testing\assertType;
+
+/**
+ * @template T
+ */
+class Foo
+{
+
+	/**
+	 * @param array<int, string> $anotherPhpDocArray
+	 * @param T $anotherTemplateProperty
+	 * @param string $bothProperty
+	 * @param array<string> $anotherBothProperty
+	 */
+	public function __construct(
+		public $noType,
+		public int $nativeIntType,
+		/** @var array<int, string> */ public $phpDocArray,
+		public $anotherPhpDocArray,
+		/** @var array<int, string> */ public array $yetAnotherPhpDocArray,
+		/** @var T */ public $templateProperty,
+		public $anotherTemplateProperty,
+		/** @var int */ public $bothProperty,
+		/** @var array<int> */ public $anotherBothProperty
+	) {
+		assertType('array<int, string>', $phpDocArray);
+		assertNativeType('mixed', $phpDocArray);
+		assertType('array<int, string>', $anotherPhpDocArray);
+		assertNativeType('mixed', $anotherPhpDocArray);
+		assertType('array<int, string>', $yetAnotherPhpDocArray);
+		assertNativeType('array', $yetAnotherPhpDocArray);
+		assertType('int', $bothProperty);
+		assertType('array<int>', $anotherBothProperty);
+	}
+
+}
+
+function (Foo $foo): void {
+	assertType('mixed', $foo->noType);
+	assertType('int', $foo->nativeIntType);
+	assertType('array<int, string>', $foo->phpDocArray);
+	assertType('array<int, string>', $foo->anotherPhpDocArray);
+	assertType('array<int, string>', $foo->yetAnotherPhpDocArray);
+	assertType('int', $foo->bothProperty);
+	assertType('array<int>', $foo->anotherBothProperty);
+};
+
+/**
+ * @extends Foo<\stdClass>
+ */
+class Bar extends Foo
+{
+
+}
+
+function (Bar $bar): void {
+	assertType('stdClass', $bar->templateProperty);
+	assertType('stdClass', $bar->anotherTemplateProperty);
+};
+
+/**
+ * @template T
+ */
+class Lorem
+{
+
+	/**
+	 * @param T $foo
+	 */
+	public function __construct(
+		public $foo
+	) { }
+
+}
+
+function (): void {
+	$lorem = new Lorem(new \stdClass);
+	assertType('stdClass', $lorem->foo);
+};
+
+/**
+ * @extends Foo<\stdClass>
+ */
+class Baz extends Foo
+{
+
+	public function __construct(
+		public $anotherPhpDocArray
+	)
+	{
+		assertType('array<int, string>', $anotherPhpDocArray);
+		assertNativeType('mixed', $anotherPhpDocArray);
+	}
+
+}
+
+function (Baz $baz): void {
+	assertType('array<int, string>', $baz->anotherPhpDocArray);
+	assertType('stdClass', $baz->templateProperty);
+};
---
+++ test.php
@@ -0,0 +1,22 @@
+<?php
+
+/** @var \Composer\Autoload\ClassLoader $loader */
+$loader = require_once __DIR__ . '/vendor/autoload.php';
+
+$errors = 0;
+if ($loader->findFile(\NewInInitializers\Foo::class) === false) {
+	echo "NewInInitializers\\Foo does not exist\n";
+	$errors++;
+}
+
+if ($loader->findFile(\PromotedPropertiesTypes\Foo::class) === false) {
+	echo "PromotedPropertiesTypes\\Foo does not exist\n";
+	$errors++;
+}
+
+if ($errors === 0) {
+	echo "Everything okay\n";
+	return;
+}
+
+exit(1);
